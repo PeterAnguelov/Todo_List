05d4ad346874d7614bc7cf725e5f3b59dd4dad4e
Peter Anguelov <pet.anguelov@gmail.com>
2025-11-20 20:41:39 +0200

    Added the Group creator option

--- Todo_List/Program.cs
+++ Todo_List/Program.cs
@@ -9,6 +9,8 @@ namespace Todo_List
     internal class Program
     {
         static List<string> tasks = new List<string>();
+        static List<string> groups = new List<string>();
+        static List<List<int>> assignedGroups = new List<List<int>>();
         static void Main(string[] args)
         {
             int option = -1;
@@ -61,27 +63,23 @@ namespace Todo_List
                 case 2:
                     CurrentTasks();
                     Console.ReadLine();
-
                     break;
                 case 3:
                     index = CompleteTask() - 1;
                     tasks[index] = tasks[index] + " (Completed)";
-
                     break;
                 case 4:
                     index = RemoveTask() - 1;
                     tasks.RemoveAt(index);
-
                     break;
                 case 5:
+                    groups.Add(CreateGroup());
                     break;
                 case 6:
                     break;
                 case 7:
                     break;
                 case 8:
-                    break;
-                case 9:
                     break;
                 default:
                     break;
@@ -91,7 +89,7 @@ namespace Todo_List
         // 1. Add a new task
         static string AddNewTask()
         {
-            Console.Write("Please task: ");
+            Console.Write("Please create a task: ");
             string task = Console.ReadLine();
 
             return task;
@@ -125,6 +123,15 @@ namespace Todo_List
             return Convert.ToInt32(Console.ReadLine());
         }
 
+        // 5. Create task Group
+        static string CreateGroup()
+        {
+            Console.Write("Please create a group: ");
+            string task = Console.ReadLine();
+            assignedGroups.Add(new List<int>());
+
+            return task;
+        }
     }
 }
 
